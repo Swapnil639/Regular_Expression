d693743b0b32c35bd34e00b192295be4514ed4db
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-12 19:09:11 +0530

    Added :User Password create with 1 uppercase,1 numeric,1 special character check if valid or not

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -25,6 +25,22 @@ public class UserRegistration {
         String mobileNumber = scanner.next();
         System.out.println(Pattern.matches("[+]91 [6-9]\\d{9}", mobileNumber));
 
+        System.out.println("Enter Password");
+        String Password1 = scanner.nextLine();
+        System.out.println(Pattern.matches("[a-z A-Z]{8,}", Password1));
+
+        System.out.println("Enter Password should have at least one upper case letter");
+        String Password2 = scanner.nextLine();
+        System.out.println(Pattern.matches("(?=.*?[A-Z])[a-z A-Z]{8,}", Password2));
+
+        System.out.println("Enter Password should have at least one numeric number in the password");
+        String Password3 = scanner.nextLine();
+        System.out.println(Pattern.matches("(?=.*?[A-Z])[a-z A-Z 0-9]{8,}", Password3));
+
+        System.out.println("Enter Password has exactly one special character");
+        String Password4 = scanner.nextLine();
+        System.out.println(Pattern.matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}", Password4));
+
     }
 
 }
