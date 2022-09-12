6110decbced92e344b67722fa51401da49b566da
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-12 19:02:03 +0530

    Added :Ability to check user's mobile number valid or not

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,22 +1,30 @@
 package com.bridgelabz;
 
+
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    static Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
-
     public static void main(String[] args) {
+        System.out.println("Welcome to user registration program");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter First Name: ");
+
+        System.out.println("Enter first name");
         String firstName = scanner.next();
         System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
-        System.out.print("Enter Last Name: ");
+
+        System.out.println("Enter last name");
         String lastName = scanner.next();
         System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
-        System.out.print("Enter E-mail: ");
-        String email = scanner.nextLine();
-        System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,5}", email));
+
+        System.out.println("Enter emailId");
+        String emailId = scanner.next();
+        System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,}", emailId));
+
+        System.out.println("Enter mobile number");
+        String mobileNumber = scanner.next();
+        System.out.println(Pattern.matches("[+]91 [6-9]\\d{9}", mobileNumber));
+
     }
+
 }
